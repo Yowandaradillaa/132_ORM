ada2bf9e9fd3336694bfcffe09fc1405f0641f7e
Yowandaradillaa <yowandaradilla13@gmail.com>
2025-10-29 11:13:56 +0700

    Jalankan server setelah sinkronisasi database Sequelize

--- index.js
+++ index.js
@@ -6,3 +6,16 @@ const db = require('./models');
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.listen(port, () => {
+    console.log(`Server started on port 3001`);
+});
+
+db.sequelize.sync()
+  .then((result) => {
+    app.listen(3001, () => {
+      console.log('Server started on port 3001');
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
