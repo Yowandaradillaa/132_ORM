60badacfc187492bc619e2cba89929a8dc2b2f2b
Yowandaradillaa <yowandaradilla13@gmail.com>
2025-10-29 11:08:25 +0700

    menambahkan setup awal express.js (bukan models)

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+const express = require('express');
+const app = express();
+const port = 3001;
+const db = require('./models');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
